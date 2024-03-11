eb6918bfc8ef32800d0f5c3fcf0b86390ae7807b
Naresh Kannan <106611387+Nareshkannan27@users.noreply.github.com>
2024-03-11 21:53:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,37 @@
 import streamlit as st
 import pickle
 
+# Load the model
+model = pickle.load(open('new_gestational.pkl', 'rb'))
 
+# Function to classify the input
+def classify(input_data):
+    # Perform classification using the loaded model
+    prediction = model.predict(input_data)
+    return prediction
 
-lin_model=pickle.load(open('lin_model.pkl','rb'))
-log_model=pickle.load(open('log_model.pkl','rb'))
-svm=pickle.load(open('svm.pkl','rb'))
-
-def classify(num):
-    if num<0.5:
-        return 'Setosa'
-    elif num <1.5:
-        return 'Versicolor'
-    else:
-        return 'Virginica'
 def main():
-    st.title("Streamlit Tutorial")
+    st.title("Gestational Diabetes Prediction")
     html_temp = """
-    <div style="background-color:teal ;padding:10px">
-    <h2 style="color:white;text-align:center;">Iris Classification</h2>
+    <div style="background-color: #F0F8FF; padding: 10px;">
+        <h2 style="color: #800000; text-align: center;">Gestational Diabetes Classification</h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
-    activities=['Linear Regression','Logistic Regression','SVM']
-    option=st.sidebar.selectbox('Which model would you like to use?',activities)
-    st.subheader(option)
-    sl=st.slider('Select Sepal Length', 0.0, 10.0)
-    sw=st.slider('Select Sepal Width', 0.0, 10.0)
-    pl=st.slider('Select Petal Length', 0.0, 10.0)
-    pw=st.slider('Select Petal Width', 0.0, 10.0)
-    inputs=[[sl,sw,pl,pw]]
+
+    # Input sliders for features
+    age = st.slider('Age', 10, 100)
+    no_of_pregnancy = st.slider('Number of Pregnancies', 0, 20)
+    # Add sliders for other features
+
+    # Collect input data
+    input_data = [[age, no_of_pregnancy]]  # Add other features here
+
     if st.button('Classify'):
-        if option=='Linear Regression':
-            st.success(classify(lin_model.predict(inputs)))
-        elif option=='Logistic Regression':
-            st.success(classify(log_model.predict(inputs)))
-        else:
-           st.success(classify(svm.predict(inputs)))
+        # Perform classification
+        result = classify(input_data)
+        # Display the result
+        st.success('Prediction: {}'.format(result))
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
